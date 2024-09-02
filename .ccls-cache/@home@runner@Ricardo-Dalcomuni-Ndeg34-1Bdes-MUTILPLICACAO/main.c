760a32b1694ec495f26f5758e2befc53e8d65ed9
Ricardo Dalcomuni <161725206+Dalcomuni@users.noreply.github.com>
2024-09-02 11:38:48 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Ricardo-Dalcomuni-Ndeg34-1Bdes-MUTILPLICACAO/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void){
+ printf("Cite dois numeros \n");
+  
+  int m;
+  int s;
+  int n;
+  
+  scanf("%d", &m);
+  scanf("%d", &s);
+  n = m * s;
+  printf("A multiplicação dos dois numeros é %d", n);
+  
+  
+  return 0;
+}
